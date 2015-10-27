a0933815bee4e53aff13a09979021f67d5e0d562
apple <apple@appledeimac.local>
2015-10-27 22:01:21 +0800

    Initial Commit

---
+++ SGWeiBo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SGWeiBo
+//
+//  Created by apple on 15/10/27.
+//  Copyright © 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SGWeiBo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SGWeiBo
+//
+//  Created by apple on 15/10/27.
+//  Copyright © 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
